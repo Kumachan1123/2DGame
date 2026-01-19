2f93d68450039e24b45a9f7d1cbff00819a6b3ec
Kumachan1123 <kumakei2004@gmail.com>
2026-01-20 05:29:42 +0900

    2Dアクションの地形生成・プレイヤー操作を作成

---
+++ Assets/Editor/ParallaxBackgroundEditor.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(ParallaxBackground))]
+public class ParallaxBackgroundEditor : Editor
+{
+    ParallaxBackground parallaxBackground;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (parallaxBackground == null)
+            parallaxBackground = target as ParallaxBackground;
+        if (GUILayout.Button("Create"))
+        {
+            parallaxBackground.CreateParallaxBackground();
+        }
+    }
+}
